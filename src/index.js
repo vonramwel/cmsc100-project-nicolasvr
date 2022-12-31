9a700285b7285323a7d6829e0a1d49df0bf406ef
vonramwel <vjnicolas@up.edu.ph>
2022-12-31 14:38:59 +0000

    refactor: move fastify to app.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-import Fastify from 'fastify';
+import { build } from './app.js';
 
 async function start () {
-    try {
-      // if something goes wrong here, it will do the "catch part"
-      const fastify = Fastify({ logger: true });
-  
-      fastify.get('/api', async (request, reply) => {
-        return { success: true };
-      });
-      
-      const addr = await fastify.listen({
-        port: '8080'
-      });
-      console.log(`Listening on ${addr}`);
-    } catch (error) {
-      // prints the error
-      console.error(error);
-      // exits the program with an error code
-      process.exit(1);
-    }
+  try {
+    // if something goes wrong here, it will do the "catch part"
+    const fastify = await build();
+
+    const addr = await fastify.listen({
+      port: '8080'
+    });
+    console.log(`Listening on ${addr}`);
+  } catch (error) {
+    // prints the error
+    console.error(error);
+    // exits the program with an error code
+    process.exit(1);
   }
-  
-  start();
+}
+
+start();
