9062311f7214f9546db32985e37e3f5eaee7d45e
vonramwel <vjnicolas@up.edu.ph>
2023-01-04 10:00:17 +0000

    feat: add a away to check if user is authenticated

--- src/services/index.js
+++ src/services/index.js
@@ -7,6 +7,7 @@ import { updateBlog } from './blogs/update-blog.js';
 import { registerUser } from './user/register-user.js';
 import { login } from './user/login.js';
 import { logout } from './user/logout.js';
+import { authCheck } from './user/auth-check.js';
 
 export class Service {
   constructor (app) {
@@ -24,4 +25,5 @@ export class Service {
    registerUser = registerUser
    login = login
    logout = logout
+   authCheck = authCheck
 }
