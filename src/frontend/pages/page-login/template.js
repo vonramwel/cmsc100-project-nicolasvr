f4aca4815aab26461b856eb577e77fafeee18aca
vonramwel <vjnicolas@up.edu.ph>
2023-01-04 17:28:57 +0000

    feat: add login

---
+++ src/frontend/pages/page-login/template.js
@@ -0,0 +1,7 @@
+import { html } from 'lit';
+// loads the login-component
+import '../../components/login-component/index.js';
+
+export function template () {
+  return html`<login-component></login-component>`;
+}
