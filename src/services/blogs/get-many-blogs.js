4272188df2905de2c607c07216385a14763b8418
vonramwel <vjnicolas@up.edu.ph>
2023-01-04 08:52:06 +0000

    fix: put the filter after sort so that limit can work

--- src/services/blogs/get-many-blogs.js
+++ src/services/blogs/get-many-blogs.js
@@ -3,12 +3,12 @@ import { getDB } from '../../utils/db/index.js';
 export const getManyBlog = async (request, reply) => {
   const { query, username } = request;
   const { limit = 5 } = query;
-  
-    // check if there is username (meaning logged in)
-    if (!username) {
-      return reply.badRequest();
-    }
-  
+
+  // check if there is username (meaning logged in)
+  if (!username) {
+    return reply.badRequest();
+  }
+
   const db = await getDB();
 
   const list = [];
@@ -23,7 +23,8 @@ export const getManyBlog = async (request, reply) => {
     })
     .sort(function (blog1, blog2) {
       return blog2.createdDate - blog1.createdDate;
-    });
+    })
+    .filter((blog) => (username === blog.username));
 
   for (const blog of blogs) {
     list.push(blog);
@@ -32,5 +33,5 @@ export const getManyBlog = async (request, reply) => {
     }
   }
 
-  return  list.filter((blog) => (username === blog.username));
+  return list;
 };
